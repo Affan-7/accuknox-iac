57a3e3cb79ffe0aaede805110a83fd6ba60b1a32
Mohammed Affan <mohammed.affan.727@gmail.com>
2024-08-29 20:56:05 +0530

    update IaC

--- main.tf
+++ main.tf
@@ -42,3 +42,21 @@ resource "aws_security_group" "insecure_sg" {
         cidr_blocks = ["0.0.0.0/0"]  # Allows traffic to anywhere
     }
 }
+
+resource "aws_security_group" "example" {
+  vpc_id = aws_vpc.example.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # This allows inbound traffic from any IP address
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
