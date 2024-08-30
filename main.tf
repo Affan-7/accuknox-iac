68aa6f6e2afbc861577020c1c8d1b3216b5f3928
Mohammed Affan <mohammed.affan.727@gmail.com>
2024-08-30 16:20:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,21 @@ provider "aws" {
     profile = "affan-accuknox-personal"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
+resource "aws_security_group" "public_sg" {
+  name        = "public_sg"
+  description = "Security group with all ports open to the public"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "-1"  # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
