0fd96cc7e9688c428e335c3812634497d6f83785
Mohammed Affan <mohammed.affan.727@gmail.com>
2024-11-26 12:15:53 +0530

    update iac code

--- main.tf
+++ main.tf
@@ -3,6 +3,43 @@ provider "aws" {
     profile = "affan-accuknox-personal"
 }
 
+resource "aws_instance" "my_instance" {
+    ami           = "ami-0c55b159cbfafe1f0"
+    instance_type = "t2.micro"
+
+    associate_public_ip_address = true
+
+    user_data = <<-EOF
+    #!/bin/bash
+    echo "AccessKey=AKIAIOSFODNN7EXAMPLE" >> /etc/creds
+    echo "SecretKey=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" >> /etc/creds
+    EOF
+
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
+
+    tags = {
+        Name = "my-instance"
+    }
+}
+
+resource "aws_security_group" "my_sg" {
+    name_prefix = "my_sg"
+
+    ingress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 resource "aws_s3_bucket" "sensitive-data2-bucket" {
   bucket = "sensitive-data2"
 }
